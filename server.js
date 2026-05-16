7e62dca6573c94dbcaa1980fc6eaff5cfb3f5da2
Srinath <gokuwoo11@gmail.com>
2026-05-16 11:06:17 +0530

    final render chrome fix

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.post("/generate-pdf", async (req, res) => {
     }
 
     const browser = await puppeteer.launch({
-      headless: true,
+      headless: "new",
       args: ["--no-sandbox", "--disable-setuid-sandbox"]
     });
 
